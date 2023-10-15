2a90c7b3f70cd4202d22d2dadc4a63bf6f5a5c37
Pal Szak <szak.pal@gmail.com>
2023-10-15 17:41:24 +0200

    Iteration: 31

--- app/src/test/java/demo/wumpus/WumpiTest.java
+++ app/src/test/java/demo/wumpus/WumpiTest.java
@@ -8,9 +8,16 @@ class WumpiTest {
   @Test
   public void theWumpiIsStinkyInTheSameRoom() {
     Wumpi wumpus = new Wumpi(new Room(5,5));
-    Player player = new Player(new Room(5,5));
-    Assertions.assertEquals(Percept.Stench, wumpus.getPerceptFrom(Room.START_POSITION), "Wumpus is stinky.");
+    Assertions.assertEquals(Percept.Stench, wumpus.getPerceptFrom(new Room(5,5)), "Wumpus is stinky.");
   }
 
+  @Test
+  public void theWumpiIsStinkyInTheAdjectiveRooms() {
+    Wumpi wumpus = new Wumpi(new Room(5,5));
+    Assertions.assertEquals(Percept.Stench, wumpus.getPerceptFrom(new Room(6,5)), "Wumpus is stinky.");
+    Assertions.assertEquals(Percept.Stench, wumpus.getPerceptFrom(new Room(4,5)), "Wumpus is stinky.");
+    Assertions.assertEquals(Percept.Stench, wumpus.getPerceptFrom(new Room(5,4)), "Wumpus is stinky.");
+    Assertions.assertEquals(Percept.Stench, wumpus.getPerceptFrom(new Room(5,6)), "Wumpus is stinky.");
+  }
 
 }
