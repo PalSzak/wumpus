5958fe4ebee6680c494b33004fda02bd19c59d19
Pal Szak <szak.pal@gmail.com>
2023-11-18 16:51:51 +0100

    Iteration: 64

---
+++ app/src/test/java/demo/wumpus/internal/ActionTest.java
@@ -0,0 +1,74 @@
+package demo.wumpus.internal;
+
+import demo.wumpus.api.Action;
+import demo.wumpus.api.Agent;
+import demo.wumpus.api.Percept;
+import demo.wumpus.internal.figures.Gold;
+import demo.wumpus.internal.figures.Player;
+import demo.wumpus.internal.figures.Wumpus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ActionTest {
+
+  @Test
+  public void goForward() {
+    GameImpl game = new GameImpl(
+        new Player(
+            Room.of(5, 5),
+            new Agent() {
+              @Override
+              public Action makeMove(List<Percept> percepts) {
+                return Action.GO_FORWARD;
+              }
+            }),
+        new WumpusWorld(
+            10,
+            new Gold(Room.START_POSITION),
+            new Wumpus(Room.START_POSITION),
+            Collections.emptyList()
+        )
+    );
+
+    game.nextRound();
+
+    Player player = game.getWorld().getFigures(Player.class).findAny().get();
+    Assertions.assertEquals(Room.of(6,5), player.getPosition(), "Go Forward");
+  }
+
+  @Test
+  void turnLeft() {
+
+    GameImpl game = new GameImpl(
+        new Player(
+            Room.of(5, 5),
+            new Agent() {
+              int i = 0;
+              @Override
+              public Action makeMove(List<Percept> percepts) {
+                return switch (++i) {
+                  case 1 -> Action.TURN_LEFT;
+                  case 2 -> Action.GO_FORWARD;
+                  default -> Action.NO_OP;
+                };
+              }
+            }),
+        new WumpusWorld(
+            10,
+            new Gold(Room.START_POSITION),
+            new Wumpus(Room.START_POSITION),
+            Collections.emptyList()
+        )
+    );
+
+    game.nextRound();
+    game.nextRound();
+
+    Player player = game.getWorld().getFigures(Player.class).findAny().get();
+    Assertions.assertEquals(Room.of(5,4), player.getPosition(), "Turn Left");
+  }
+
+}
