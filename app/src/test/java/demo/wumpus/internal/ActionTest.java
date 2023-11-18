cc4b75ed6b2f2eb38c47960a5ff465d3c46674ac
Pal Szak <szak.pal@gmail.com>
2023-11-18 16:54:02 +0100

    Iteration: 65

--- app/src/test/java/demo/wumpus/internal/ActionTest.java
+++ app/src/test/java/demo/wumpus/internal/ActionTest.java
@@ -16,22 +16,12 @@ public class ActionTest {
 
   @Test
   public void goForward() {
-    GameImpl game = new GameImpl(
-        new Player(
-            Room.of(5, 5),
-            new Agent() {
-              @Override
-              public Action makeMove(List<Percept> percepts) {
-                return Action.GO_FORWARD;
-              }
-            }),
-        new WumpusWorld(
-            10,
-            new Gold(Room.START_POSITION),
-            new Wumpus(Room.START_POSITION),
-            Collections.emptyList()
-        )
-    );
+    GameImpl game = buildGame(new Agent() {
+      @Override
+      public Action makeMove(List<Percept> percepts) {
+        return Action.GO_FORWARD;
+      }
+    });
 
     game.nextRound();
 
@@ -42,27 +32,17 @@ public class ActionTest {
   @Test
   void turnLeft() {
 
-    GameImpl game = new GameImpl(
-        new Player(
-            Room.of(5, 5),
-            new Agent() {
-              int i = 0;
-              @Override
-              public Action makeMove(List<Percept> percepts) {
-                return switch (++i) {
-                  case 1 -> Action.TURN_LEFT;
-                  case 2 -> Action.GO_FORWARD;
-                  default -> Action.NO_OP;
-                };
-              }
-            }),
-        new WumpusWorld(
-            10,
-            new Gold(Room.START_POSITION),
-            new Wumpus(Room.START_POSITION),
-            Collections.emptyList()
-        )
-    );
+    GameImpl game = buildGame(new Agent() {
+      int i = 0;
+      @Override
+      public Action makeMove(List<Percept> percepts) {
+        return switch (++i) {
+          case 1 -> Action.TURN_LEFT;
+          case 2 -> Action.GO_FORWARD;
+          default -> Action.NO_OP;
+        };
+      }
+    });
 
     game.nextRound();
     game.nextRound();
@@ -71,4 +51,17 @@ public class ActionTest {
     Assertions.assertEquals(Room.of(5,4), player.getPosition(), "Turn Left");
   }
 
+  private GameImpl buildGame(Agent agent) {
+    return new GameImpl(
+        new Player(
+            Room.of(5, 5),
+            agent),
+        new WumpusWorld(
+            10,
+            new Gold(Room.START_POSITION),
+            new Wumpus(Room.START_POSITION),
+            Collections.emptyList()
+        )
+    );
+  }
 }
